941eadaf0ef1e3be869654ec8daa5a7253825cde
amartyasurve <amartyasurve@gmail.com>
2026-03-04 17:10:31 +0530

    first commit

--- main.cpp
+++ main.cpp
@@ -1,37 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <set>
-#include <queue>
-#include <deque>
-#include <cmath>
 
+#include<iostream>
+#include<thread>
 using namespace std;
 
-typedef long long ll;
-typedef std::vector<int> vi;
-typedef std::vector<ll> vll;
-typedef std::pair<int, int> pii;
-
-// Fast I/O
-void fast_io()
+void workerfunction()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+    cout << "hello from first thread" << "\n";
 }
+
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    fast_io();
-    int a, b;
-    cin >> a >> b;
-    cout << "hello world";
-
+    thread mythread(workerfunction);
+    mythread.join();
     return 0;
 }
