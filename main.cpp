57be5972caaf6e6375ca141a7d0bd022da890dc7
amartyasurve <amartyasurve@gmail.com>
2026-03-04 16:17:03 +0530

    firstcommit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+typedef std::pair<int, int> pii;
+
+// Fast I/O
+void fast_io()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast_io();
+    int a, b;
+    cin >> a >> b;
+    cout << "hello world";
+
+    return 0;
+}
